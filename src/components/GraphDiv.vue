7bac79aee6f5426c0ceaebf0491071c4d551104c
TONYDESKTOP\Tony <tonyatpeking@gmail.com>
2024-05-04 15:55:56 -0500

    Use Default Vue Volar Formatting, Change app name

--- src/components/GraphDiv.vue
+++ src/components/GraphDiv.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref,onMounted } from "vue";
+import { ref, onMounted } from "vue";
 //import { invoke } from "@tauri-apps/api/tauri";
 import openmoji_data from '../assets/data/openmoji.json';
 import special_cases from '../assets/data/special-cases.json';
@@ -26,38 +26,37 @@ for (let i = 0; i < count; i++) {
   const emoji_data = openmoji_data[i]
   let emoji = emoji_data.emoji
   let annotation = emoji_data.annotation;
-  if( annotation == "")
-  {
+  if (annotation == "") {
     console.log(`WARNING empty annotation ${emoji}`)
   }
-  else if( annotation.includes(":")){
+  else if (annotation.includes(":")) {
     //console.log(`Variation ${emoji}`)
     continue
   }
-  if (emoji_data.subgroups == "skin-tone"){
+  if (emoji_data.subgroups == "skin-tone") {
     continue;
   }
-  if (emoji_data.subgroups == "regional-indicator"){
+  if (emoji_data.subgroups == "regional-indicator") {
     continue;
   }
-  
+
 
 
   // add zero width joiner if missing
   // is compatible if this gets updated
-  if(special_cases.missing_zero_width_joiner.includes(emoji) ){
+  if (special_cases.missing_zero_width_joiner.includes(emoji)) {
     emoji = emoji.replace(ZERO_WIDTH_JOINER, "")
     emoji = [...emoji].join(ZERO_WIDTH_JOINER)
   }
-  else if(special_cases.missing_font.includes(emoji) ){
+  else if (special_cases.missing_font.includes(emoji)) {
     continue;
   }
   str += emoji
   valid_count += 1
   //console.log(openmoji_data[i].hexcode)
   if (valid_count % 15 == 0) {
-  str += "\n"
-}
+    str += "\n"
+  }
 }
 
 
@@ -69,104 +68,69 @@ const gData = {
     .filter(id => id)
     .map(id => ({
       source: id,
-      target: Math.round(Math.random() * (id-1))
+      target: Math.round(Math.random() * (id - 1))
     }))
 };
 
 
 // SVG test
-function loadSVG( url: string, scene: THREE.Scene) {
-
-
-console.log(url);
-//
-
-const helper = new THREE.GridHelper( 160, 10, 0x8d8d8d, 0xc1c1c1 );
-helper.rotation.x = Math.PI / 2;
-scene.add( helper );
-
-//
-
-const loader = new SVGLoader();
-
-loader.load( url, function ( data ) {
-
-  const group = new THREE.Group();
-  group.scale.multiplyScalar( 0.25 );
-  group.position.x = - 70;
-  group.position.y = 70;
-  group.scale.y *= - 1;
-
-  console.log(url)
-  console.log(data)
-
-  let renderOrder = 0;
-
-  for ( const path of data.paths ) {
-    if( !path.userData ){
-      continue;
-    }
-    const fillColor = path.userData.style.fill;
-
-    if ( fillColor !== undefined && fillColor !== 'none' ) {
-
-      const material = new THREE.MeshBasicMaterial( {
-        color: new THREE.Color().setStyle( fillColor ),
-        opacity: path.userData.style.fillOpacity,
-        transparent: true,
-        side: THREE.DoubleSide,
-        depthWrite: false
-      } );
-
-      const shapes = SVGLoader.createShapes( path );
-
-      for ( const shape of shapes ) {
-
-        const geometry = new THREE.ShapeGeometry( shape );
-        const mesh = new THREE.Mesh( geometry, material );
-        mesh.renderOrder = renderOrder ++;
-
-        group.add( mesh );
-
+function loadSVG(url: string, scene: THREE.Scene) {
+  console.log(url);
+  const helper = new THREE.GridHelper(160, 10, 0x8d8d8d, 0xc1c1c1);
+  helper.rotation.x = Math.PI / 2;
+  scene.add(helper);
+  const loader = new SVGLoader();
+  loader.load(url, function (data) {
+    const group = new THREE.Group();
+    group.scale.multiplyScalar(0.25);
+    group.position.x = - 70;
+    group.position.y = 70;
+    group.scale.y *= - 1;
+    console.log(url)
+    console.log(data)
+    let renderOrder = 0;
+    for (const path of data.paths) {
+      if (!path.userData) {
+        continue;
       }
-
-    }
-
-    const strokeColor = path.userData.style.stroke;
-
-    if ( strokeColor !== undefined && strokeColor !== 'none' ) {
-
-      const material = new THREE.MeshBasicMaterial( {
-        color: new THREE.Color().setStyle( strokeColor ),
-        opacity: path.userData.style.strokeOpacity,
-        transparent: true,
-        side: THREE.DoubleSide,
-        depthWrite: false
-      } );
-
-      for ( const subPath of path.subPaths ) {
-
-        const geometry = SVGLoader.pointsToStroke( subPath.getPoints(), path.userData.style );
-
-        if ( geometry ) {
-
-          const mesh = new THREE.Mesh( geometry, material );
-          mesh.renderOrder = renderOrder ++;
-
-          group.add( mesh );
-
+      const fillColor = path.userData.style.fill;
+      if (fillColor !== undefined && fillColor !== 'none') {
+        const material = new THREE.MeshBasicMaterial({
+          color: new THREE.Color().setStyle(fillColor),
+          opacity: path.userData.style.fillOpacity,
+          transparent: true,
+          side: THREE.DoubleSide,
+          depthWrite: false
+        });
+        const shapes = SVGLoader.createShapes(path);
+        for (const shape of shapes) {
+          const geometry = new THREE.ShapeGeometry(shape);
+          const mesh = new THREE.Mesh(geometry, material);
+          mesh.renderOrder = renderOrder++;
+          group.add(mesh);
         }
-
       }
-
+      const strokeColor = path.userData.style.stroke;
+      if (strokeColor !== undefined && strokeColor !== 'none') {
+        const material = new THREE.MeshBasicMaterial({
+          color: new THREE.Color().setStyle(strokeColor),
+          opacity: path.userData.style.strokeOpacity,
+          transparent: true,
+          side: THREE.DoubleSide,
+          depthWrite: false
+        });
+        for (const subPath of path.subPaths) {
+          const geometry = SVGLoader.pointsToStroke(subPath.getPoints(), path.userData.style);
+          if (geometry) {
+            const mesh = new THREE.Mesh(geometry, material);
+            mesh.renderOrder = renderOrder++;
+            group.add(mesh);
+          }
+        }
+      }
     }
-
-  }
-
-  scene.add( group );
-
-
-} );
+    scene.add(group);
+  });
 
 }
 
@@ -176,10 +140,10 @@ let canvas_div = ref<HTMLDivElement>();
 
 onMounted(() => {
   const Graph = ForceGraph3D()
-  (canvas_div.value!)
+    (canvas_div.value!)
     .graphData(gData);
   console.log(Graph.height());
-  loadSVG( tigerUrl, Graph.scene());
+  loadSVG(tigerUrl, Graph.scene());
 })
 
 
@@ -188,7 +152,7 @@ onMounted(() => {
 
 <template>
   <div ref="canvas_div"></div>
-  <div class="openmoji-color">{{str}}</div>
+  <div class="openmoji-color">{{ str }}</div>
   <p>{{ greetMsg }}</p>
 </template>
 
@@ -197,6 +161,7 @@ onMounted(() => {
   font-family: OpenMojiColorFont;
   src: url('../assets/fonts/OpenMoji-color-glyf_colr_0.woff2') format('woff2');
 }
+
 @font-face {
   font-family: OpenMojiBlack;
   src: url('../assets/fonts/OpenMoji-black-glyf.woff2') format('woff2');
@@ -206,6 +171,7 @@ onMounted(() => {
   white-space: pre-line;
   font-family: OpenMojiColorFont, sans-serif;
 }
+
 .openmoji-black {
   font-family: OpenMojiBlack, sans-serif;
 }
